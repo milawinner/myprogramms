0c49fab6e8e15b77f325692f7eae097bed2ead74
Mila Winner <112131879+milawinner@users.noreply.github.com>
2022-09-01 13:50:04 +0000

    Create Programm003.cs

---
+++ Programm003.cs
@@ -0,0 +1,11 @@
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Это целое число");
+}
+else
+{
+    Console.WriteLine("Это число не является целым");
+}
